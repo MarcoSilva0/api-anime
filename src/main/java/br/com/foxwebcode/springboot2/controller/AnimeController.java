4c3c1cfda60eb0c6fe19a012d080096beab9b892
MarcoSilva0 <marcoantonio02016@outlook.com>
2024-10-20 19:28:47 -0300

    feat: add anime endpoint getById

--- src/main/java/br/com/foxwebcode/springboot2/controller/AnimeController.java
+++ src/main/java/br/com/foxwebcode/springboot2/controller/AnimeController.java
@@ -3,17 +3,11 @@ package br.com.foxwebcode.springboot2.controller;
 import br.com.foxwebcode.springboot2.domain.Anime;
 import br.com.foxwebcode.springboot2.service.AnimeService;
 import br.com.foxwebcode.springboot2.util.DateUtil;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -24,8 +18,14 @@ public class AnimeController {
     private final DateUtil dateUtil;
     private final AnimeService animeService;
 
-    @GetMapping()
-    public List<Anime> list() {
-        return animeService.listAll();
+    @GetMapping
+    public ResponseEntity<List<Anime>> list() {
+        return ResponseEntity.ok(animeService.listAll());
+    }
+
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id) {
+        return ResponseEntity.ok(animeService.findById(id));
     }
 }
